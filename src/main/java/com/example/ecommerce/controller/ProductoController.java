686f4b7f45a572abb79b248d87b6a4af4ae716c8
Enzo Sierra <enzogsierra@gmail.com>
2022-04-04 22:41:47 -0300

    Añadido funcionalidad de crear productos

--- src/main/java/com/example/ecommerce/controller/ProductoController.java
+++ src/main/java/com/example/ecommerce/controller/ProductoController.java
@@ -1,8 +1,15 @@
 package com.example.ecommerce.controller;
 
+import com.example.ecommerce.model.Producto;
+import com.example.ecommerce.model.Usuario;
+import com.example.ecommerce.service.ProductoService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -10,9 +17,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/productos")
 public class ProductoController 
 {
-    @GetMapping("/")
+    @Autowired
+    private ProductoService productoService;
+    
+    
+    @GetMapping("")
     public String index(Model model)
     {
         return "productos/index";
+    }
+    
+    @GetMapping("/crear")
+    public String create(Model model)
+    {
+        return "productos/crear";
+    }
+    
+    @PostMapping("/crear")
+    public String create_POST(Producto producto) throws JsonProcessingException
+    {
+        /*ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(producto);
+        System.out.println("Producto = " + json);*/
+        
+        Usuario usuario = new Usuario(1, "", "", "", "", "", "", "", "");
+        producto.setUsuario(usuario);
+        productoService.save(producto);
+        
+        return "redirect:/productos";
     }
 }
