2555b79de856d22a846d90d04e6c65d64d391e6c
Enzo Sierra <enzogsierra@gmail.com>
2022-04-18 10:40:36 -0300

    metodo para generar numero de orden

--- src/main/java/com/example/ecommerce/service/IOrdenService.java
+++ src/main/java/com/example/ecommerce/service/IOrdenService.java
@@ -1,9 +1,11 @@
 package com.example.ecommerce.service;
 
 import com.example.ecommerce.model.Orden;
+import java.util.List;
 
 
 public interface IOrdenService 
 {
+    List<Orden> all();
     void save(Orden orden);
 }
--- src/main/java/com/example/ecommerce/service/OrdenServiceImpl.java
+++ src/main/java/com/example/ecommerce/service/OrdenServiceImpl.java
@@ -2,6 +2,9 @@ package com.example.ecommerce.service;
 
 import com.example.ecommerce.model.Orden;
 import com.example.ecommerce.repository.OrdenRepository;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +15,33 @@ public class OrdenServiceImpl implements IOrdenService
     @Autowired
     private OrdenRepository ordenRepository;
     
+    
+    @Override
+    public List<Orden> all() {
+        return ordenRepository.findAll();
+    }
+    
     @Override
     public void save(Orden orden) {
         ordenRepository.save(orden);
+    }
+    
+    
+    public String generateOrderNumber()
+    {
+        int n;
+        List<Orden> ordenes = all();
+        
+        if(ordenes.isEmpty()) n = 0;
+        else
+        { 
+            List<Integer> numbers = new ArrayList<>();
+            
+            ordenes.stream().forEach(orden -> numbers.add(Integer.parseInt(orden.getNumero())));
+            n = numbers.stream().max(Integer::compare).get() + 1;
+        }
+        
+        String str = new DecimalFormat("0000000000").format(n);
+        return str;
     }
 }
